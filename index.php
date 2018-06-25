102ede35b10a728fa72b07ff672cd791d1211e43
joversky <joversky@yahoo.fr>
2018-06-24 22:35:04 -0400

    tes
    tes

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
+
+$app->get('/cowsay', function() use($app) {
+  $app['monolog']->addDebug('cowsay');
+  return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
+});
+
+
 // Message Vars
 $msg = '';
 $msgClass = '';
